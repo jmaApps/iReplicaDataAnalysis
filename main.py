b8f1dd82748bc5dbc0902ed7073b3e9515386878
CookieCoder17 <ak06192003@gmail.com>
2023-02-06 15:42:18 -0500

    First commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     '''
         uncomment the method(s) that you would like to execute
     '''
-    read_inbox()
+    # read_inbox()
     # clear_datasheets()
-    # read_csv()
-    # visualizeData()
+    read_csv()
+    visualizeData()
